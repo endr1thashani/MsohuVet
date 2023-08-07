808ee293134543b3220d7610ae6c2eae7133e456
endr1thashani <136480302+endr1thashani@users.noreply.github.com>
2023-08-08 01:44:20 +0200

    Update TKuiz.js

--- src/Pages/Kuizet/TKuiz.js
+++ src/Pages/Kuizet/TKuiz.js
@@ -122,7 +122,7 @@ const TKuiz = () => {
               </>
             ) : (
               <>
-              <div className='text-slate-700 text-[16px] font-semibold'>
+              <div className='text-slate-700 text-[12px] md:text-[16px] font-semibold'>
                   <div className='m-4'>
                     <span>Pytjet {currentQuestion + 1}</span>/{allQuestions.length}
                   </div>
